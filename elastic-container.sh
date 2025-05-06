ae76e87c602b2bdd808425d31dccbbb92fe85358
Eric Ooi <54374877+eric-ooi@users.noreply.github.com>
2025-05-06 13:31:29 -0500

    Add files via upload

--- elastic-container.sh
+++ elastic-container.sh
@@ -2,6 +2,7 @@
 set -o pipefail
 
 ipvar="0.0.0.0"
+servername=$(hostname)
 
 # These should be set in the .env file
 declare LinuxDR
@@ -28,19 +29,6 @@ passphrase_reset() {
   else
     echo "Passphrase has been reset. Proceeding."
   fi
-}
-
-check_required_apps() {
-    apps=("jq" "curl")
-
-    for app in "${apps[@]}"; do
-        if ! command -v "$app" &>/dev/null; then
-            echo "The application '$app' is not installed."
-            exit 1
-        fi
-    done
-
-    echo "All required applications are installed."
 }
 
 # Create the script usage menu
@@ -98,7 +86,7 @@ configure_kbn() {
 
           curl -k --silent "${HEADERS[@]}" --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X POST "${LOCAL_KBN_URL}/api/detection_engine/rules/_bulk_action" -d'
             {
-              "query": "alert.attributes.tags: (\"Linux\" OR \"OS: Linux\")",
+              "query": "alert.attributes.tags: \"Linux\"",
               "action": "enable"
             }
             ' 1>&2
@@ -109,7 +97,7 @@ configure_kbn() {
 
           curl -k --silent "${HEADERS[@]}" --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X POST "${LOCAL_KBN_URL}/api/detection_engine/rules/_bulk_action" -d'
             {
-              "query": "alert.attributes.tags: (\"Windows\" OR \"OS: Windows\")",
+              "query": "alert.attributes.tags: \"Windows\"",
               "action": "enable"
             }
             ' 1>&2
@@ -120,7 +108,7 @@ configure_kbn() {
 
           curl -k --silent "${HEADERS[@]}" --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X POST "${LOCAL_KBN_URL}/api/detection_engine/rules/_bulk_action" -d'
             {
-              "query": "alert.attributes.tags: (\"macOS\" OR \"OS: macOS\")",
+              "query": "alert.attributes.tags: \"macOS\"",
               "action": "enable"
             }
             ' 1>&2
@@ -152,20 +140,9 @@ get_host_ip() {
 }
 
 set_fleet_values() {
-  # Get the current Fleet settings
-  CURRENT_SETTINGS=$(curl -k -s -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X GET "${LOCAL_KBN_URL}/api/fleet/agents/setup" -H "Content-Type: application/json")
-
-  # Check if Fleet is already set up
-  if echo "$CURRENT_SETTINGS" | grep -q '"isInitialized": true'; then
-    echo "Fleet settings are already configured."
-    return
-  fi
-
-  echo "Fleet is not initialized, setting up Fleet..."
-  
-  fingerprint=$(${COMPOSE} exec -w /usr/share/elasticsearch/config/certs/ca elasticsearch cat ca.crt | openssl x509 -noout -fingerprint -sha256 | cut -d "=" -f 2 | tr -d :)
-  printf '{"host_urls": ["%s"], "name": "default", "is_default": true}' "https://${ipvar}:${FLEET_PORT}" | curl -k --silent --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPOST "${HEADERS[@]}" "${LOCAL_KBN_URL}/api/fleet/fleet_server_hosts" -d @- | jq
-  printf '{"hosts": ["%s"]}' "https://${ipvar}:9200" | curl -k --silent --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPUT "${HEADERS[@]}" "${LOCAL_KBN_URL}/api/fleet/outputs/fleet-default-output" -d @- | jq
+  fingerprint=$(${COMPOSE} exec -w /usr/share/elasticsearch/config/certs/ca es01 cat ca.crt | openssl x509 -noout -fingerprint -sha256 | cut -d "=" -f 2 | tr -d :)
+  printf '{"fleet_server_hosts": ["%s"]}' "https://${servername}:${FLEET_PORT}" | curl -k --silent --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPUT "${HEADERS[@]}" "${LOCAL_KBN_URL}/api/fleet/settings" -d @- | jq
+  printf '{"hosts": ["%s"]}' "https://${servername}:9200" | curl -k --silent --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPUT "${HEADERS[@]}" "${LOCAL_KBN_URL}/api/fleet/outputs/fleet-default-output" -d @- | jq
   printf '{"ca_trusted_fingerprint": "%s"}' "${fingerprint}" | curl -k --silent --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPUT "${HEADERS[@]}" "${LOCAL_KBN_URL}/api/fleet/outputs/fleet-default-output" -d @- | jq
   printf '{"config_yaml": "%s"}' "ssl.verification_mode: certificate" | curl -k --silent --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPUT "${HEADERS[@]}" "${LOCAL_KBN_URL}/api/fleet/outputs/fleet-default-output" -d @- | jq
   policy_id=$(printf '{"name": "%s", "description": "%s", "namespace": "%s", "monitoring_enabled": ["logs","metrics"], "inactivity_timeout": 1209600}' "Endpoint Policy" "" "default" | curl -k --silent --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPOST "${HEADERS[@]}" "${LOCAL_KBN_URL}/api/fleet/agent_policies?sys_monitoring=true" -d @- | jq -r '.item.id')
@@ -229,21 +206,19 @@ case "${ACTION}" in
   # Collect the Elastic, Kibana, and Elastic-Agent Docker images
   docker pull "docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION}"
   docker pull "docker.elastic.co/kibana/kibana:${STACK_VERSION}"
-  docker pull "docker.elastic.co/beats/elastic-agent:${STACK_VERSION}"
+  docker pull "docker.elastic.co/elastic-agent/elastic-agent:${STACK_VERSION}"
   ;;
 
 "start")
   passphrase_reset
 
-  check_required_apps
-
   get_host_ip
 
   echo "Starting Elastic Stack network and containers."
 
-  ${COMPOSE} up -d --no-deps 
+  ${COMPOSE} up -d --no-deps
 
-  configure_kbn 1>&2 2>&3
+  #configure_kbn 1>&2 2>&3
 
   echo "Waiting 40 seconds for Fleet Server setup."
   echo
@@ -252,20 +227,19 @@ case "${ACTION}" in
 
   echo "Populating Fleet Settings."
   set_fleet_values > /dev/null 2>&1
+  #set_fleet_values
   echo
 
   echo "READY SET GO!"
   echo
-  echo "Browse to https://localhost:${KIBANA_PORT}"
-  echo "Username: ${ELASTIC_USERNAME}"
-  echo "Passphrase: ${ELASTIC_PASSWORD}"
+  echo "Browse to https://${servername}:${KIBANA_PORT}"
   echo
   ;;
 
 "stop")
   echo "Stopping running containers."
 
-  ${COMPOSE} stop 
+  ${COMPOSE} stop
   ;;
 
 "destroy")
@@ -279,7 +253,7 @@ case "${ACTION}" in
   echo "#####"
   echo "Restarting all Elastic Stack components."
   echo "#####"
-  ${COMPOSE} restart elasticsearch kibana fleet-server
+  ${COMPOSE} restart es01 es02 es03 kibana fleet-server
   ;;
 
 "status")
